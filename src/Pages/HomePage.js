ee57f08fafe8fb682394182f78cb344e5b1d5823
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-08-15 03:55:37 +0300

    fix/ order of post is now by Time of upload (newer is in top)

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -11,7 +11,7 @@ const HomePage = () => {
     const [posts, setPosts] = useState([]);
 
     useEffect(()=>{
-        db.collection('posts').onSnapshot(snapshot=>{
+        db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot=>{
             setPosts(snapshot.docs.map(doc=>({
                 id: doc.id,
                 post: doc.data()

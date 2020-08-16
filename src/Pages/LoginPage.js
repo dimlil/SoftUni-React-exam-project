70176e39b0dcff17aeb49409eae6db64b16234a3
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-08-16 03:47:14 +0300

    fiz/ remove unusing hook

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import styles from '../styles/loginAndRegister.module.css'
 import {Link} from 'react-router-dom'
 import { auth } from '../firebase'
